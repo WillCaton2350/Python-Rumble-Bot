d3efd164ecafc7d5d064f5a7731a90b685a68cc2
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2024-07-21 23:14:42 -0400

    Delete Python-Rumble-Bot-Final/bot/src.py

--- Python-Rumble-Bot-Final/bot/src.py
+++
@@ -1,9 +0,0 @@
-from bot.main import web_driver
-
-class source:
-    def activate(self):
-        func = web_driver()
-        func.start_driver()
-        func.start_browser()
-        func.quit_driver()
-        print(f'node: web_driver activated')
