2ac849a776481855c57bf6556820641cb80faa15
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2024-07-30 23:30:21 -0400

    Update root.py

--- Python-Rumble-Bot-Final/root.py
+++ Python-Rumble-Bot-Final/root.py
@@ -1,31 +1,19 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as WDW
 from selenium.common.exceptions import NoSuchElementException
-from undetected_chromedriver.options import ChromeOptions
 from selenium.webdriver.common.by import By
-import undetected_chromedriver as uc
 from urllib.error import HTTPError
+from seleniumbase import Driver
 from random import randint
 from config.data import *
 import logging
-import ssl
 
 class web_driver:
     def __init__(self):
         self.driver = None
     
     def start_driver(self):
-        ssl._create_default_https_context = ssl._create_unverified_context
-        options = ChromeOptions()
-        options.add_argument("--single-process")
-        options.add_argument("--headless")
-        options.add_argument("--incognito")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-cache")
-        options.add_argument("--disable-blink-features=AutomationControlled")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--mute-audio")
-        self.driver = uc.Chrome(options=options)
+        self.driver = Driver(uc=True)
         user_agent = useragents.ua.random
         self.driver.execute_cdp_cmd(
         f"Network.setUserAgentOverride",
@@ -49,34 +37,14 @@ class web_driver:
                         time_altered.time_buffer(2)
                     except NoSuchElementException as err:
                         if err:
-                            logging.error(f'NoSuchElementException: {err}')
-                            logging.info(f"Retrying Element in {nums.retry_delay} seconds...")
-                            nums.retry_delay = self.fibonacci(nums.retry_delay)
+                            logging.error(err)
                             nums.retries += 1
                     break 
                 except HTTPError as err:
                     if err.code != nums.status:
-                        logging.error(f"HTTP error occurred: {err}")
-                        logging.info(f"Retrying in {nums.retry_delay} seconds...")
-                        nums.retry_delay = self.fibonacci(nums.retry_delay)
+                        logging.error(err)
                         nums.retries += 1
                         
-    def fibonacci(n):
-        if n <= 0:
-            return []
-        elif n == 1:
-            return [0]
-        elif n == 2:
-            return [0, 1]
-        else:
-            sequence = [0, 1]
-            for i in range(2, n):
-                random_increment = randint(1, 5)
-                next_number = sequence[-1] + sequence[-2] + random_increment
-                sequence.append(next_number)
-            return sequence
-    n = 10  
-    fibonacci_sequence = fibonacci(n)
 
     def quit_driver(self):
         self.driver.close()
