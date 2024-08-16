661e855c3d8920ed1cdc8efa505f01d6297b501b
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2024-08-16 11:37:53 -0400

    Update root.py

--- Python-Rumble-Bot/root.py
+++ Python-Rumble-Bot/root.py
@@ -2,22 +2,25 @@ from bot.main import web_driver
 from config.data import nums
 
 class node:
-    def __init__(self,data=None):
-        self.data = data
+    def __init__(self,_):
+        self.data = self.activate()
         self.left = None
         self.right = None
 
-class binary_tree:
     def activate(self):
         func = web_driver()
         func.start_driver()
         func.start_browser()
         func.close_browser()
 
+class binary_tree:
+    global stack
+    global result
+    stack = []
+    result = []
+
     def main(self, root:node):
         # in order traversal
-        stack = []
-        result = []
         while root is not None or stack != []:
             while root is not None:
                 stack.append(root)
@@ -30,17 +33,17 @@ class binary_tree:
         return result
 
 if __name__ == "__main__":
-    instance = binary_tree()
-    root = node(1)
-    root.left = node(2)
-    root.right = node(3)
-    root.left.left = node(4)
-    root.left.right = node(5)
-    root.right.left = node(6)
-    root.right.right = node(7)
     try:
+        instance = binary_tree()
+        root = node(1)
+        root.left = node(2)
+        root.right = node(3)
+        root.left.left = node(4)
+        root.left.right = node(5)
+        root.right.left = node(6)
+        root.right.right = node(7)
         for i in range(nums.num_nodes):
-            val = instance.main(root)
+            traversal = instance.main(root=root)
             print(f'Traversal Pattern: {val}')
             print(f'Execution: {i}')
     except KeyboardInterrupt:
