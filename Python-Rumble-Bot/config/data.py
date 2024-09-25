fa837d5537093c7f944f3456be670a7647f22dc3
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2024-09-25 14:14:03 -0400

    Update data.py

--- Python-Rumble-Bot/config/data.py
+++ Python-Rumble-Bot/config/data.py
@@ -14,28 +14,10 @@ class nums:
 
 @dataclass
 class urls:
-    main_url: str = 'https://rumble.com'
+    main_url: str = 'https://www.example.com'
 
     url_dict = {
-    #1:'https://rumble.com/v4krwyl-traversal.html',
-    2:'https://rumble.com/v4krxo9-pandas.html',
-    3:'https://rumble.com/v4krxw6-checkers.html',
-    4:'https://rumble.com/v4krzqc-ayeagain.html',
-    5:'https://rumble.com/v4ks25h-broddin.html',
-    6:'https://rumble.com/v4ks44q-frezo.html',
-    7:'https://rumble.com/v4ks54r-fact.html',
-    8:'https://rumble.com/v4ks5sr-graded.html',
-    9:'https://rumble.com/v4ks600-heart-army.html',
-    10:'https://rumble.com/v4ks6q9-breathe.html',
-    11:'https://rumble.com/v4ks6xk-need4speed.html',
-    #12:'https://rumble.com/v4ks7de-oogami.html',
-    13: 'https://rumble.com/v4ks8f9-tera.html',
-    14: 'https://rumble.com/v4ks8m0-lamn.html',
-    15: 'https://rumble.com/v4ks8y9-tintroduction.html',
-    #16: 'https://rumble.com/v4ksc0h-twitter-beat.html',
-    #17: 'https://rumble.com/v4ksdfr-soubbin.html',
-    18:'https://rumble.com/v4kvo2c-ranked.html',
-    19: 'https://rumble.com/v4lboxl-regenerate.html'
+    1:'https:/www.example.com',
     }
 
 class locators:
