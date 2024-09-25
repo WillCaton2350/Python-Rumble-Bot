7787624723f1981bb597baa8764ffae6296295e5
WJC_Jr <54005049+WillCaton2350@users.noreply.github.com>
2024-09-25 15:35:25 -0400

    Add files via upload

---
+++ Python-Rumble-Bot-Final-Bot/bot/main.py
@@ -0,0 +1,47 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait as WDW
+from selenium.common.exceptions import NoSuchElementException
+from config.data import urls, locators, useragents, time_buffer
+from selenium.webdriver.common.by import By
+from urllib.error import HTTPError
+from seleniumbase import Driver 
+import logging
+
+class web_driver:
+    def __init__(self):
+        self.driver = None
+    
+    def start_driver(self):
+        self.driver = Driver(uc=True)
+        user_agent = useragents.ua.random
+        self.driver.execute_cdp_cmd(
+        f"Network.setUserAgentOverride",
+        {"userAgent":user_agent})
+        return self.driver
+
+    def start_browser(self):
+        for i in urls.url_dict:
+            value = urls.url_dict[i]
+            try:
+                self.driver.get(value)
+                WDW(self.driver, 10).until(
+                EC.url_to_be(value))
+                try:
+                    WDW(self.driver,10).until(
+                    EC.presence_of_element_located((
+                        By.CLASS_NAME,locators.play_btn_classname
+                    )))
+                    self.driver.find_element(
+                    By.CLASS_NAME,locators.play_btn_classname).click()
+                    time_buffer.time_altered(1)
+                except NoSuchElementException as err:
+                    if err:
+                        logging.error(f'NoSuchElementException: {err}')
+
+            except HTTPError as err:
+                if err.code != 200:
+                    logging.error(f"HTTP error occurred: {err}")
+        
+        
+    def close_browser(self):
+        self.driver.quit()
---
+++ Python-Rumble-Bot-Final-Bot/root.py
@@ -0,0 +1,44 @@
+from bot.main import web_driver
+from config.data import nums
+
+class node:
+    def __init__(self,_):
+        self.data = self.activate()
+        self.left = None
+        self.right = None
+
+    def activate(self):
+        for i in range(nums.num_nodes):
+            func = web_driver()
+            func.start_driver()
+            func.start_browser()
+            func.close_browser()
+            
+
+
+class binary_tree:
+    global stack
+    global result
+    stack = []
+    result = []
+
+    def main(self, root:node):
+        # in order traversal
+        while root is not None or stack != []:
+            while root is not None:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            result.append(root.data)
+            root = root.right
+            print(f'Node: {node}')
+        return result
+
+if __name__ == "__main__":
+    try:
+        instance = binary_tree()
+        root = node(1)
+    except KeyboardInterrupt:
+        print("exiting gracefully...")
+        exit(0)
+        
